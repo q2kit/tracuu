486ba1237c4380129325d6fa3cd34b7b861bc586
Nguyễn Thế Anh <86357279+theanhgk16@users.noreply.github.com>
2024-07-03 14:29:42 +0700

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class SearchForm(forms.Form):
     tax_code = forms.CharField(max_length=50, label='Tax Code', widget=forms.TextInput(attrs={
-        'placeholder': 'Vui lòng nhập mã số thuế',
+        'placeholder': 'Vui lòng nhập mã tra cứu',
         'class': 'form-control'
     }))
