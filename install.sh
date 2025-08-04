f0ca0233915b377f3b2194376398e610d27e3269
Doãn Hữu Quân <67795391+q2kit@users.noreply.github.com>
2025-08-04 20:21:20 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+echo "==> Đang cài đặt server..."
+
+if ! command -v nginx >/dev/null 2>&1; then
+  echo "==> Đang cài nginx..."
+  apt update -y
+  apt install -y nginx
+else
+  echo "==> Nginx đã được cài."
+fi
+
+NGINX_CONF="/etc/nginx/sites-available/tracuu"
+if [ ! -f "$NGINX_CONF" ]; then
+  tee "$NGINX_CONF" > /dev/null <<EOF
+server {
+    listen 80;
+    server_name tracuuhoadon247.com;
+
+    location / {
+        proxy_pass http://localhost:8000;
+        proxy_set_header    Host \$host;
+        proxy_set_header    X-Real-IP \$remote_addr;
+        proxy_set_header    X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}
+EOF
+else
+  echo "==> File cấu hình nginx đã tồn tại."
+fi
+
+if [ ! -L /etc/nginx/sites-enabled/tracuu ]; then
+  ln -s /etc/nginx/sites-available/tracuu /etc/nginx/sites-enabled/
+fi
+
+nginx -t
+systemctl restart nginx
+
+echo "==> Pull Docker image..."
+docker pull q2kit/tracuu
+
+mkdir -p /srv/tracuu/media
+touch /srv/tracuu/db.sqlite3
+chown -R $(whoami):$(whoami) /srv/tracuu
+
+if docker ps -a --format '{{.Names}}' | grep -Eq '^tracuu$'; then
+  docker stop tracuu
+  docker rm tracuu
+fi
+
+docker run -d -p 8000:80 --name tracuu \
+  -v /srv/tracuu/db.sqlite3:/srv/db.sqlite3 \
+  -v /srv/tracuu/media:/srv/media \
+  q2kit/tracuu
+
+echo "✅ Hoàn tất cài đặt!"
