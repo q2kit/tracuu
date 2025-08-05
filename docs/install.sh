f013cf07b6b0599029ef80dd635c2a967fb963d0
Doãn Hữu Quân <q2k@q2k.dev>
2025-08-05 15:59:55 +0700

    feat: install docs: docker

--- docs/install.sh
+++ docs/install.sh
@@ -45,6 +45,26 @@ fi
 nginx -t
 systemctl restart nginx
 
+if ! command -v docker >/dev/null 2>&1; then
+  echo "==> Docker chưa được cài. Đang tiến hành cài đặt..."
+  apt update -y
+  apt install -y ca-certificates curl gnupg lsb-release
+
+  install -m 0755 -d /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  chmod a+r /etc/apt/keyrings/docker.gpg
+
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  apt update -y
+  apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  echo "==> Docker đã được cài đặt."
+fi
+
+# Chuẩn bị thư mục chứa dữ liệu
 echo "==> Pull Docker image..."
 docker pull q2kit/tracuu
 
