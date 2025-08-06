0e3fe7fda6ad4a1ae5af7718f557f2631bc47dd9
Doãn Hữu Quân <q2k@q2k.dev>
2025-08-06 16:20:18 +0700

    feat: r2 storage

--- app/models.py
+++ app/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Product(models.Model):
     tax_code = models.CharField(max_length=100, unique=True, verbose_name="Mã số thuế")
-    product_name = models.CharField(max_length=255, unique=False, verbose_name="Tên sản phẩm")
-    image = models.ImageField(upload_to='images/', unique=True, verbose_name="Ảnh mã số thuế")
+    product_name = models.CharField(max_length=255, verbose_name="Tên sản phẩm")
+    image = models.ImageField(upload_to="images/", verbose_name="Ảnh mã số thuế")
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.tax_code
-
     class Meta:
-        ordering = ['-created_at']
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return self.tax_code
