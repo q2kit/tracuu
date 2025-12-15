1b95ef487c293d242ad8b77331bea2926125cb41
Doan Huu Quan <q2k@q2k.dev>
2025-12-15 15:44:21 +0700

    Merge branch 'main' of github.com:q2kit/tracuu into development

--- project/settings.py
+++ project/settings.py
@@ -12,13 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-import dotenv
-
 from project.const import ENV, LOCAL_ENV, SERVER_HOST
-
-# Load environment variables from .env file
-dotenv.load_dotenv()
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
