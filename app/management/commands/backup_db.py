685464a789b3fc64c9d9915e71786bbe36e094b8
Doan Huu Quan <q2k@q2k.dev>
2025-12-18 22:29:30 +0700

    feat: upgrade project to v2.0

--- app/management/commands/backup_db.py
+++ app/management/commands/backup_db.py
@@ -8,7 +8,7 @@ from django.utils import timezone
 
 
 class Command(BaseCommand):
-    help = "Backup db.sqlite3 to R2 bucket"
+    help = "Backup db.sqlite3 to S3 bucket"
 
     def handle(self, *args, **options):  # noqa: ARG002
         db_path = os.path.join(settings.BASE_DIR, "db.sqlite3")
@@ -19,20 +19,20 @@ class Command(BaseCommand):
         now = timezone.localtime().strftime("%Y%m%d_%H%M%S")
         backup_key = f"db_backup/{now}.sqlite3"
 
-        # Get R2/S3 credentials from settings
+        # Get S3 credentials from settings
         endpoint_url = settings.AWS_S3_ENDPOINT_URL
-        access_key = settings.AWS_ACCESS_KEY_ID
-        secret_key = settings.AWS_SECRET_ACCESS_KEY
-        bucket_name = settings.AWS_STORAGE_BUCKET_NAME
         region_name = settings.AWS_S3_REGION_NAME
+        access_key = settings.AWS_S3_ACCESS_KEY_ID
+        secret_key = settings.AWS_S3_SECRET_ACCESS_KEY
+        bucket_name = settings.AWS_STORAGE_BUCKET_NAME
 
-        # Create S3 client for R2
+        # Create S3 client
         s3 = boto3.client(
             "s3",
             endpoint_url=endpoint_url,
+            region_name=region_name,
             aws_access_key_id=access_key,
             aws_secret_access_key=secret_key,
-            region_name=region_name,
         )
 
         try:
