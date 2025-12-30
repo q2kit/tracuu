62551cc42651a26ad581e50fcc2d4c6e9ed39415
Doãn Hữu Quân <q2k@q2k.dev>
2025-12-30 13:39:24 +0700

    fix: s3 bug

--- src/funcs.py
+++ src/funcs.py
@@ -13,6 +13,7 @@ def generate_presigned_url(
     s3_client = boto3.client(
         "s3",
         endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+        region_name=settings.AWS_S3_REGION_NAME,
         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
     )
