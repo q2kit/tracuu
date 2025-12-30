11a11c7fe2f1c2dca1d15669efb5f94918510071
Doãn Hữu Quân <67795391+q2kit@users.noreply.github.com>
2025-12-30 12:21:51 +0700

    Client's Feedback (#27)
    Co-authored-by: Doan Huu Quan <q2k@q2k.dev>

--- src/management/commands/backup_db.py
+++ src/management/commands/backup_db.py
@@ -25,7 +25,6 @@ class Command(BaseCommand):
 
             # Get S3 credentials from settings
             endpoint_url = settings.AWS_S3_ENDPOINT_URL
-            region_name = settings.AWS_S3_REGION_NAME
             access_key = settings.AWS_ACCESS_KEY_ID
             secret_key = settings.AWS_SECRET_ACCESS_KEY
             bucket_name = settings.AWS_STORAGE_BUCKET_NAME
@@ -34,7 +33,6 @@ class Command(BaseCommand):
             s3 = boto3.client(
                 "s3",
                 endpoint_url=endpoint_url,
-                region_name=region_name,
                 aws_access_key_id=access_key,
                 aws_secret_access_key=secret_key,
             )
